3110f03b591c3fb7f080910d2f991985dd887609
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2018-12-07 15:49:34 -0200

    Add files via upload

---
+++ conversionenmonedas1.js
@@ -0,0 +1,36 @@
+module.exports = function coinConvert(usDollars) {
+
+
+  var arr = [];
+  var peruvianSoles = usDollars * 3.25;
+  
+  var mexicanPesos = usDollars * 18 ;
+  
+  var chileanPesos = usDollars * 660;
+  
+
+  console.log(peruvianSoles, mexicanPesos, chileanPesos); 
+ 
+
+	 // Añade el monto equivalente en soles
+  
+	arr.push(peruvianSoles);
+
+  
+
+	// Añade el monto equivalente en pesos mexicanos
+  
+	arr.push(mexicanPesos);
+
+  
+
+	// Añade el monto equivalente en pesos chilenos
+
+	  arr.push(chileanPesos);
+
+ 
+
+return arr;
+
+
+};
