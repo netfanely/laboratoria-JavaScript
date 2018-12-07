dcae72256ebcf7bce128938e0f0db60dfd42a5fd
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2018-12-07 16:00:40 -0200

    Add files via upload

---
+++ restaurante1.js
@@ -0,0 +1,26 @@
+module.exports = function restaurantBill(bill) {
+  
+
+// 1. Crea una variable llamada tax y asígnale el resultado de multiplicar
+// bill por 10%.
+  
+
+	var tax  = bill*0.10;
+
+  
+
+// 2. Crea una variable llamada total y asígnale el resultado de sumar bill
+// más tax
+ 
+
+	var total = bill + tax;
+
+  
+
+// 3. Retorna el monto que cada una debe pagar, con el símbolo $ adelante
+  
+
+	return '$'+total/5;
+
+
+};
